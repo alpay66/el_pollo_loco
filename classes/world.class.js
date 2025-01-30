65363dfe4f65af7d2b01c766c0d7cf497665292a
BigPoppa <allpay_66_@hotmail.de>
2025-01-30 01:20:05 +0100

    update

--- classes/world.class.js
+++ classes/world.class.js
@@ -38,16 +38,14 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.level.backgroundObjects);
-
-
+        /////////////////
         this.ctx.translate(-this.camera_x, 0);
         // ------- space for fixed objects
         this.addToMap(this.healthBar);
         this.addToMap(this.coinBar);
         this.addToMap(this.bottleBar);
         this.ctx.translate(this.camera_x, 0); 
-
-
+        /////////////////
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.coins);
