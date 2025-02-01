f8e49b5f0445bb08d021a319c10f47cedc0e48ed
BigPoppa <allpay_66_@hotmail.de>
2025-02-01 19:16:10 +0100

    update

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,30 +1,3 @@
-/* class ThrowableObject extends MovableObject {
-    ROTATED_SALSA_BOTTLE = [
-        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
-        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
-    ];
-
-    constructor(x, y) {
-        super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png'); // Ruft den Konstruktor von MovableObject auf
-        this.x = x;
-        this.y = y;
-        this.height = 90;
-        this.width = 70;
-        this.throwBottle();
-    }
-
-    throwBottle() {
-        this.speedY = 30;
-        this.applyGravity();
-        setInterval(( ) => {
-            this.x += 10;
-        }, 25);
-    }
-}
- */
-
 class ThrowableObject extends MovableObject {
     ROTATED_SALSA_BOTTLE = [
         'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
@@ -35,7 +8,7 @@ class ThrowableObject extends MovableObject {
 
     // speedX: horizontale Fluggeschwindigkeit, rotationSpeed: wie schnell die Bilder wechseln (Rotation)
     constructor(x, y, speedX = 7, rotationSpeed = 1) {
-        // Startbild laden
+
         super().loadImage('img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png');
         this.x = x;
         this.y = y;
