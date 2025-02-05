87d12569093deaff6f22fc6835bc6634ccbbc8cb
BigPoppa <allpay_66_@hotmail.de>
2025-02-05 18:25:57 +0100

    update

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -7,15 +7,18 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
+    CHICKEN_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.CHICKEN_WALKING);
+        this.loadImages(this.CHICKEN_DEAD);
+        this.animateChicken();
 
         this.x = 190 + Math.random() * 400;
         this.speed = 0.15 + Math.random() * 0.5;
-
-        this.animateChicken();
     }
     
     animateChicken () {
--- classes/small-chicken-class.js
+++ classes/small-chicken-class.js
@@ -7,14 +7,17 @@ class SmallChicken extends MovableObject {
         'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
+    SMALL_CHICKEN_DEAD = [
+        'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png',);
         this.loadImages(this.SMALL_CHICKEN_WALKING);
-
+        this.loadImages(this.SMALL_CHICKEN_DEAD);
         this.x = 190 + Math.random() * 400;
         this.speed = 0.15 + Math.random() * 1;
-
+        
         this.animateSmallChicken();
     }
     
