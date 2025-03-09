47acd8e033bcd751bfa62688eab2f3cdb7a80eae
alpay66 <allpay_66_@hotmail.de>
2025-03-09 22:37:36 +0100

    rotate responsive overlay fixed

--- js/events.js
+++ js/events.js
@@ -1,6 +1,6 @@
 function checkOrientation() {
     let rotateWarning = document.getElementById("rotate-warning");
-    if (window.innerHeight > window.innerWidth) {
+    if (window.innerHeight > window.innerWidth && window.innerWidth <= 920) {
         rotateWarning.style.display = "flex";
     } else {
         rotateWarning.style.display = "none";
