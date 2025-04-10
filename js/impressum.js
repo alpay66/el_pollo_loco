21e579d089249eefc2e030f7609281d0aee59e03
alpay66 <allpay_66_@hotmail.de>
2025-04-10 19:31:20 +0200

    impressum injected :D

--- js/impressum.js
+++ js/impressum.js
@@ -1,4 +1,10 @@
 function toggleImpressum() {
-    const imp = document.getElementById("impressum");
-    imp.style.display = imp.style.display === "none" ? "block" : "none";
+    const impressum = document.getElementById("impressum");
+    const currentDisplay = window.getComputedStyle(impressum).display;
+
+    if (currentDisplay === "none") {
+        impressum.style.display = "block";
+    } else {
+        impressum.style.display = "none";
+    }
 }
