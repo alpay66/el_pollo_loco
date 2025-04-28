fa553bc088e0087b890c7faedb5f81044c4643c8
alpay66 <allpay_66_@hotmail.de>
2025-04-28 19:51:08 +0200

    finish maybe 4

--- levels/level_1.js
+++ levels/level_1.js
@@ -12,33 +12,33 @@ function levelInit() {
     level_1 = new Level(
         // Gegner im Level
         [
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn 
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new SmallChicken(),  // Ein kleines Huhn
-            new Chicken(),       // Ein normales Huhn
-            new Endboss()        // Der Endboss 
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),        
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new SmallChicken(),  
+            new Chicken(),       
+            new Endboss()        
         ],
         // Wolken im Level
         [
-            new Cloud(),        // Eine Wolke
+            new Cloud(),
         ],
         // Hintergrundobjekte im Level
         [
@@ -65,25 +65,25 @@ function levelInit() {
         ],
         // Münzen im Level
         [
-            new Coin(200, 2900, 300), // Münze an Position (300, 2000) mit einem Wert von 300
-            new Coin(400, 2400, 250), // Münze an Position (300, 2000) mit einem Wert von 250
-            new Coin(300, 2600, 200),  // Münze an Position (300, 2000) mit einem Wert von 200
-            new Coin(500, 3100, 350),  // Münze an Position (300, 2000) mit einem Wert von 350
-            new Coin(200, 2900, 300),  // Münze an Position (300, 2000) mit einem Wert von 300
-            new Coin(600, 2900, 250),  // Münze an Position (300, 2000) mit einem Wert von 250
-            new Coin(400, 2800, 200),  // Münze an Position (300, 2000) mit einem Wert von 200
-            new Coin(800, 2900, 350),  // Münze an Position (300, 2000) mit einem Wert von 350
+            new Coin(200, 2900, 300),
+            new Coin(400, 2400, 250),
+            new Coin(300, 2600, 200),
+            new Coin(500, 3100, 350),
+            new Coin(200, 2900, 300),
+            new Coin(600, 2900, 250),
+            new Coin(400, 2800, 200),
+            new Coin(800, 2900, 350),
         ],
         // Flaschen im Level
         [
-            new Bottle(200, 2900),  // Flasche an Position (200, 2000)
-            new Bottle(250, 3500),  // Flasche an Position (250, 2000)
-            new Bottle(200, 2900),  // Flasche an Position (300, 2000)
-            new Bottle(350, 2900),  // Flasche an Position (350, 2000)
-            new Bottle(600, 2900),  // Flasche an Position (400, 2000)
-            new Bottle(450, 2500),  // Flasche an Position (450, 2000)
-            new Bottle(820, 2600),  // Flasche an Position (320, 2000)
-            new Bottle(980, 2700)   // Flasche an Position (180, 2600)
+            new Bottle(200, 2900),  
+            new Bottle(250, 3500),  
+            new Bottle(200, 2900),  
+            new Bottle(350, 2900),  
+            new Bottle(600, 2900),  
+            new Bottle(450, 2500),  
+            new Bottle(820, 2600),  
+            new Bottle(980, 2700)   
         ],
     );
 }
